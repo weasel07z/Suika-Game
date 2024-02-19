49be8f29699922ee4f6138be7eb894f7eb66242b
weasel07z <95734049+weasel07z@users.noreply.github.com>
2024-02-19 10:35:40 -0600

    please

--- sketch.js
+++ sketch.js
@@ -30,7 +30,7 @@ var _yoffset = 20
 // var slider = document.getElementById("gravitySlider");
 // var sliderText = document.getElementById("sliderValue")
 // sliderText.textContent = slider.value;
-var gravity = 10;
+var gravity = 0.9;
 
 // POINTS - SELF EXPLANETORY
 var points = 0;
